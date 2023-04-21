a15c7234d173bcdb4e27c1bc7b485d81ac92ab06
Veeresh R G <veeresh.ravi@gmail.com>
2023-04-21 12:30:37 +0530

    [EDIT] Generate Data

--- generator.py
+++ generator.py
@@ -17,17 +17,72 @@
         hotelId,
         createdAt,
         status,
-        payment: 'cod' | 'online' | 'wallet', 
-    }
-    
-    orderUpdate:{
+        payment: 'cod' | 'online' | 'wallet'
+
+        orderUpdate:{
         orderId,
-        status,
+        status : 'order-confirm' | 'cooking' | "waiting-for-delivery" | "out-for-delivery" | "delivered" | "cancelled" | "refund",
         deliveryBoyName,
         deliveryBoyPhone
+        }
     }
-   
-   
     
     
 """
+
+import random
+import datetime
+import time
+
+payment = ["cod", "online", "wallet"]
+status = ["order-confirm", "cooking", "waiting-for-delivery",
+          "out-for-delivery", "delivered", "cancelled", "refund"]
+
+
+def generateOrders():
+
+    data = []
+    # generate 1000 orders
+    for i in range(1000):
+        order = {}
+        order["orderId"] = i
+        order["userId"] = random.randint(1, 100)
+        order["items"] = []
+        for j in range(random.randint(1, 5)):
+            item = {}
+            item["itemId"] = random.randint(1, 100)
+            item["priceItem"] = random.randint(1, 100)
+            item["categoryName"] = "categoryName"
+            item["itemName"] = "itemName"
+            item["quantity"] = random.randint(1, 5)
+            order["items"].append(item)
+        order["netPrice"] = random.randint(1, 100)
+        order["discountPrice"] = random.randint(1, 100)
+        order["hotelName"] = "hotelName"
+        order["hotelId"] = random.randint(1, 100)
+        order["createdAt"] = datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S")
+
+        # randomise status
+        order["status"] = status[random.randint(0, 6)]
+        order["payment"] = payment[random.randint(0, 2)]
+
+        # randomise orderUpdate
+        order["orderUpdate"] = {}
+        order["orderUpdate"]["orderId"] = i
+        order["orderUpdate"]["status"] = status[random.randint(0, 6)]
+        # generate random names
+        order["orderUpdate"]["deliveryBoyName"] = "DeliveryBoyName " + \
+            str(random.randint(1, 100))
+
+        # generate 10 digit random phone numebr
+        order["orderUpdate"]["deliveryBoyPhone"] = random.randint(
+            1000000000, 9999999999)
+
+        data.append(order)
+
+    return data
+
+
+data = generateOrders()
+print(data)
