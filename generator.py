8939ee74afaca75004bfd342bde1e0a5eda9cc6a
Ankur-Datta-4 <ankurr2002@gmail.com>
2023-04-22 16:04:36 +0530

    [UPDATE] order-update

--- generator.py
+++ generator.py
@@ -16,7 +16,7 @@
         hotelName,
         hotelId,
         createdAt,
-        status,
+         status:  
         payment: 'cod' | 'online' | 'wallet', 
     }
     
@@ -36,7 +36,7 @@
 
 import json
 import time
-from random import randint
+from random import randint, uniform, choice
 # from kafka import KafkaProducer
 from faker import Faker
 import math
@@ -50,7 +50,8 @@ ORDER_UPDATE_TOPIC = 'order_updates'
 COORDINATES_TOPIC = 'coordinates'
 
 # producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
+generatedOrders=dict()
+# outForDeliveryOrders=dict()
 def generate_order():
     items = []
     for _ in range(randint(1, 3)):
@@ -70,35 +71,75 @@ def generate_order():
         else:
             category = 'Chinese'
         price = round(fake.random_number(digits=3, fix_len=True), 2)
-        items.append({"category": category, "name": item_name, "price": price})
+        items.append({"category": category, "name": item_name, "price": price, "quantity": randint(1,3)})
 
+    netPrice=sum(item['price'] for item in items)
+    hotelId=randint(1, 100)
+    hotelName=f"Hotel {hotelId}"
+    
     order = {
-        "order_id": fake.uuid4(),
-        "customer_id": fake.uuid4(),
-        "hotel_id": fake.uuid4(),
+        "orderId": fake.uuid4(),
+        "customerId": fake.uuid4(),
+        "hotelId": hotelId,
+        "hotelName": hotelName,
         "items": items,
         "timestamp": int(time.time()),
-        "discountPrice": round(fake.random_number(digits=3, fix_len=True), 2),
-        "hotelName": fake.company(),
-        "hotelId": fake.uuid4(),
+        "netPrice":netPrice,
+        "discountPrice": round(uniform(0, netPrice/2), 2),
         "createdAt": fake.date_time_this_month(before_now=True, after_now=False, tzinfo=None),
         "payment": fake.random_element(elements=('cod', 'online', 'wallet'))
     }
+    
+    generatedOrders[order['orderId']]={"orderId":order['orderId'],"status":"pending-confirmation"}
     return order
 
 
-def generate_order_update(order_id):
-    delivery_boy_name = fake.name()
-    delivery_boy_phone_number = fake.phone_number()
+def generate_order_update():
+    """
+        Sequence:
+            1. pending-confirmation
+            2. order-confirm
+            3. cooking
+            4. waiting-for-delivery
+            5. out-for-delivery => {deliveryBoyName, deliveryBoyPhone}
+            6. delivered
+            
+        Cancel:
+            1. pending-confirmation
+            2. order-cancelled
+            3. refund
+    """
+    orderIds=list(generatedOrders.keys())
+    orderId=choice(orderIds)
+    currentStatus=generatedOrders[orderId]['status']
+    newStatus=""
+    if currentStatus in ["pending-confirmation","order-confirm","cooking","waiting-for-delivery","out-for-delivery"]:
+        if currentStatus=="pending-confirmation":
+            update=choice(["order-confirm","order-cancelled"])
+            newStatus=newStatus+update
+        elif currentStatus=="order-confirm":
+            newStatus=newStatus+"cooking"
+        elif currentStatus=="cooking":
+            newStatus=newStatus+"waiting-for-delivery"
+        elif currentStatus=="waiting-for-delivery":
+            newStatus=newStatus+"out-for-delivery"
+            if "deliveryBoyName" not in generatedOrders[orderId]:
+                generatedOrders[orderId]["deliveryBoyName"]=fake.name()
+            if "deliveryBoyPhone" not in generatedOrders[orderId]:
+                generatedOrders[orderId]["deliveryBoyPhone"]=fake.phone_number()
+            # generatedOrders[orderId]['status']=newStatus
+            # outForDeliveryOrders[orderId]=generatedOrders[orderId]
+        elif currentStatus=="out-for-delivery":
+            newStatus=newStatus+"delivered"
+    elif currentStatus in ["delivered", "refund"]:
+        # remove order from generatedOrders
+        generatedOrders.pop(orderId)
+        return None
+    elif currentStatus =="order-cancelled":
+        newStatus="refund"
 
-    update = {
-        "order_id": order_id,
-        "status": fake.random_element(elements=('order-confirm', 'cooking', 'waiting for captain', 'order-cancelled', 'shipped', 'delivered')),
-        "timestamp": int(time.time()),
-        "deliveryBoyName": delivery_boy_name,
-        "deliveryBoyPhoneNumber": delivery_boy_phone_number
-    }
-    return update
+    generatedOrders[orderId]["status"]=newStatus
+    return generatedOrders[orderId]
 
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # Earth's radius in kilometers
@@ -145,15 +186,23 @@ while True:
     order = generate_order()
     print ("order" , order)
 
-    order_update = generate_order_update(order['order_id'])
-    print("ORDER_UPDATE_TOPIC", order_update)
+    order_update = generate_order_update()
+    if(order_update is not None):
+        print("ORDER_UPDATE_TOPIC", order_update)
 
-    customer_latitude = fake.latitude()
-    customer_longitude = fake.longitude()
+    order_update = generate_order_update()
+    if(order_update is not None):
+        print("ORDER_UPDATE_TOPIC", order_update)
+        
+    order_update = generate_order_update()
+    if(order_update is not None):
+        print("ORDER_UPDATE_TOPIC", order_update)
+    # customer_latitude = fake.latitude()
+    # customer_longitude = fake.longitude()
 
-    # Generate the next coordinate and update the previous coordinates
-    coordinates, prev_latitude, prev_longitude = generate_coordinates(customer_latitude, customer_longitude, prev_latitude, prev_longitude)
-    print("COORDINATES_TOPIC", coordinates)
+    # # Generate the next coordinate and update the previous coordinates
+    # coordinates, prev_latitude, prev_longitude = generate_coordinates(customer_latitude, customer_longitude, prev_latitude, prev_longitude)
+    # print("COORDINATES_TOPIC", coordinates)
 
     # Sleep for a few seconds
     time.sleep(randint(1, 5))
